fff6df8f375096a798447818e029ccc752e777ba
xu <xu@00fcee48-bca6-446a-b9d5-5d3b22c4d7dc>
2012-11-19 17:31:43 +0000

    Some problems with getKbdString() method =(
    git-svn-id: svn://127.0.0.1/var/cvs/xu/AddressBook/trunk@178 00fcee48-bca6-446a-b9d5-5d3b22c4d7dc

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -12,191 +10,131 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-
-
 public class AddressBook {
-	
-	public AddressBook() throws IOException {
-		f = new File(filePath);
-//		contacts = getList();
-		contacts = getContactListFromFile();
+
+	private AddressBook() {
 	}
-	
-	private String filePath = ".//book.txt";
+
+	private String filePath = ".//addressbook.txt";
 	private File f;
-//	private LinkedList<String> contacts;
 	private LinkedList<Contact> contacts;
-	
+
+	public static AddressBook getInstance() throws IOException {
+		AddressBook ab = new AddressBook();
+		ab.f = new File(ab.filePath);
+		ab.contacts = ab.getContactListFromFile();
+		return ab;
+	}
+
 	public void add(String[] args) throws IOException {
-		
-//		if (! ArgsVerifier.verifyName(args[0])) {
-//			System.out.println("Name may not be empty");
-//			return;
-//		}
-//		
-//		if (! ArgsVerifier.verifyPhone(args[1])) {
-//			System.out.println("Incorrect phone number");
-//			return;
-//		}
-//		
-//		if (! ArgsVerifier.verifyEmail(args[2])) {
-//			System.out.println("Incorrect E-Mail address");
-//			return;
-//		}
-		
 		Contact c = new Contact(args[0], args[1], args[2]);
 		addContact(c);
 	}
-	
-//	public List<String> getAllContacts() {
-	public List<Contact> getAllContacts() {
-		Collections.sort(contacts);
-		return contacts;
-	}
-	
+
 	private void addContact(Contact c) throws IOException {
-		
+
 		if (search(c.getName()) != null) {
 			System.out.println("Contact " + c.getName() + " already exists.");
 			return;
 		}
-		
-		contacts.add(c);
-		
-//		History h = new History();
-//		h.addContact(c);
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//		try {
-//			c.serialize(baos);
 
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		String data = new String(baos.toByteArray());
-		String data = c.serialize();
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new BufferedWriter(new FileWriter(f, true)));
 
-		PrintWriter out1 = new PrintWriter(new BufferedWriter(new FileWriter(f,
-				true)));
-		out1.println(data);
-		out1.close();
-		
+			out.println(c.serialize());
+			contacts.add(c);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 	}
-	
-//	private LinkedList<String> getList() throws IOException {
-//	
-//		LinkedList<String> contactStringList = new LinkedList<String>();
-//		
-//		for (Contact c : getContactList()) {
-//			contactStringList.add(c.toString());
-//		}
-//		
-//		return contactStringList;
-//		
-//	}
-	
+
+	public List<Contact> getAllContacts() {
+		Collections.sort(contacts);
+		return contacts;
+	}
+
 	private LinkedList<Contact> getContactListFromFile() throws IOException {
-		if (!f.exists()){
+		if (!f.exists()) {
 			f.createNewFile();
 		}
-			
-		LinkedList<Contact> contactList = new LinkedList<Contact>();
-//		History hnew = new History();
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				new FileInputStream(f)));
-		while (reader.ready()) {
-//			hnew.deserialize(new ByteArrayInputStream(reader.readLine()
-//					.getBytes()));
-			contactList.add(Contact.deserialize(reader.readLine()));
+		LinkedList<Contact> contactList = new LinkedList<Contact>();
+
+		BufferedReader reader = null;
+
+		try {
+			reader = new BufferedReader(new InputStreamReader(
+					new FileInputStream(f)));
+			while (reader.ready()) {
+				contactList.add(Contact.deserialize(reader.readLine()));
+			}
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 
-//		contactList = (LinkedList<Contact>) hnew.getRecords();
-		reader.close();
-		
 		return contactList;
-		
-	}
-	
-//	public String search (String name) {
-//		List<Contact> contactList = getContactList();
-//		String searchResult = "";
-//		
-//		for (Contact c : contactList) {
-//			if (c.getName().equals(name)) {
-//				searchResult = c.toString();
-//				break;
-//			}
-//		}
-//		if (searchResult.length() == 0) {
-//			System.out.println("Contact " + name + " not found");
-//		}
-//		return searchResult;
-//	}
-	
-	public Contact search (String name) throws IOException {
 
+	}
+
+	public Contact search(String name) {
 		for (Contact c : contacts) {
 			if (c.getName().equals(name)) {
 				return c;
 			}
 		}
-		
-//		System.out.println("Contact " + name + " not found");
-			
 		return null;
 	}
-	
-	public boolean delete (String name) throws IOException {
-		
+
+	public boolean delete(String name) throws IOException {
+
 		Contact toDelete = search(name);
-		
+
 		if (toDelete == null) {
-			
+
 			System.out.println("No contact with name " + name + " found.");
 			return false;
 		}
-		
-		contacts.remove(toDelete);
-		
-		
-//		History h = new History();
-		
-//		for (Contact c : contacts) {
-//			h.addContact(c);
-//		}
 
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		
-//		try {
-//			h.serialize(baos);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		String data = new String(baos.toByteArray());
-		
-		PrintWriter out1 = new PrintWriter(new BufferedWriter(new FileWriter(f,
-				false)));
-		
-		
-		if (contacts.size() == 0){
-			out1.println("");
+		contacts.remove(toDelete);
+
+		PrintWriter out = null;
+
+		try {
+			out = new PrintWriter(new BufferedWriter(new FileWriter(f, false)));
+
+			for (Contact c : contacts) {
+				out.println(c.serialize());
+			}
 		}
-		
-		for (Contact c : contacts) {
-			out1.println(c.serialize());
+
+		finally {
+			if (out != null) {
+				out.close();
+			}
 		}
-		
-		out1.close();
-		
+
 		return true;
 	}
-	
-	public void printBook (List<Contact> contacts) throws IOException {
-		for (Contact c: contacts) {
+
+	public void printBook() {
+
+		if (contacts.size() == 0) {
+			System.out.println("Addressbook is empty.");
+		}
+
+		for (Contact c : contacts) {
 			System.out.println(c.toString() + "\n");
 		}
 	}
-	
-	
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,140 +1,175 @@
 import java.io.IOException;
 
-
 public class Main {
 
+	public static void main(String[] args) {
 
-	public static void main(String[] args) throws IOException {
-		AddressBook ab = new AddressBook();
-		
-		
-		while(true)
-		{    
-		  System.out.println("Enter command.");
-		  String s = new String(getKbdString());
-//		  System.out.println("String: " + s);
-		      
-		  if(s.equals("exit")) {
-		    break;
-		  }
-		  
-		  if (s.startsWith("add")) {
-			  String name = "";
-			  String phone = "";
-			  String email = "";
-			  
-				  System.out.print("Enter name: ");
-				  name = new String(getKbdString());
-				  
-				  while (! ArgsVerifier.verifyName(name)) {
-					  System.out.print("Name may not be empty. Enter name: ");
-					  name = new String(getKbdString());
-					}
+		AddressBook ab = null;
+		try {
+			ab = AddressBook.getInstance();
+		} catch (IOException e) {
+			System.out.println("Failed to load addressbok.");
+			System.exit(1);
+		}
 
-				  System.out.print("Enter phone: ");
-				  phone = new String(getKbdString());
-				  
-				while (! ArgsVerifier.verifyPhone(phone)) {
-						 System.out.print("Incorrect phone number. Enter phone: ");
-						 phone = new String(getKbdString());
+		while (true) {
+			System.out.println("Enter command.");
+			String s = new String(getKbdString());
+//			System.out.println("s" + s);
+			if (s.equals("exit")) {
+				break;
+			}
+
+			if (s.equals("add")) {
+				String name = "";
+				String phone = "";
+				String email = "";
+
+				System.out.println("Enter name: ");
+				name = new String(getKbdString());
+
+				while (!ArgsVerifier.verifyName(name)) {
+					System.out.println("Name may not be empty. Enter name: ");
+					name = new String(getKbdString());
+				}
+
+				System.out.println("Enter phone: ");
+				phone = new String(getKbdString());
+
+				while (!ArgsVerifier.verifyPhone(phone)) {
+					System.out.println("Incorrect phone number. Enter phone: ");
+					phone = new String(getKbdString());
+				}
+
+				System.out.println("Enter email: ");
+				email = new String(getKbdString());
+
+				while (!ArgsVerifier.verifyEmail(email)) {
+					System.out
+							.println("Incorrect E-Mail address. Enter email: ");
+					email = new String(getKbdString());
+				}
+
+				try {
+					ab.add(new String[] { name, phone, email });
+					System.out.println("Contact " + name + ", " + phone + ", "
+							+ email + " saved.");
+				} catch (IOException e) {
+					System.out.println("Failed to add contact.");
+				}
+				continue;
+
+			}
+
+			if (s.startsWith("delete")) {
+
+				if (s.length() > 6 && s.charAt(6) != ' ') {
+					System.out.println("Unknown command.");
+					printHelp();
+					continue;
+				}
+
+				String name = s.substring(6).trim();
+				if (name.equals("")) {
+					System.out.println("Command format: delete <name>.");
+					continue;
+				}
+
+				try {
+					if (ab.delete(name)) {
+						System.out.println("Contact " + name
+								+ " successfully deleted.");
+						continue;
 					}
-				  
-				  System.out.print("Enter email: ");
-				  email = new String(getKbdString());
-				  
-					while (! ArgsVerifier.verifyEmail(email)) {
-						System.out.println("Incorrect E-Mail address. Enter email: ");
-						email = new String(getKbdString());
-					}
-				  
-				  ab.add(new String[]{name, phone, email});
-				  
-				  System.out.println("Contact " + name + ", " + phone + ", " + email + " saved.");
-		  }
-		    
-		  if (s.startsWith("delete")) {
-			  String name = s.substring(6).trim();
-			  if (name.equals("")) {
-				  System.out.println("Command format: delete <name>.");
-				  continue;
-			  }
-			  
-			  if (ab.delete(name))	{
-				  System.out.println("Contact " + name + " successfully deleted.");
-				  continue;
-			  }
-			  
-			  System.out.println("Contact " + name + " not found.");
-		  }
-		  
-		  if(s.equals("get")) {
-			  ab.printBook(ab.getAllContacts());
-		  }
-		  
-		  if(s.startsWith("search")) {
-			  String name = s.substring(6).trim();
-			  
-			  System.out.println(s);
-			  
-			  if (name.equals("")) {
-				  System.out.println("Command format: search <name>.");
-				  continue;
-			  }
-			  
-			  Contact c = ab.search(name);
-			  
-			  if (c == null) {
-				  System.out.println("Contact " + name + " not found.");
-				  continue;
-			  }
-			  System.out.println(c.toString());
-		  }
-		  
-		  if(s.equals("help")) {
-			  System.out.println("Available commands (enter parameters without brackets):");
-			  System.out.println("add - creates new contact.");
-			  System.out.println("delete <name> - deletes specified contact from addressbook.");
-			  System.out.println("search <name> - searches for specified contact.");
-			  System.out.println("get - shows list of all contacts.");
-			  System.out.println("exit - exits program.");
-		  }
-		  
-		  
-		}  
-		
-		
-		
-//		ab.add(new String[]{"zacd", "991aa", "aaa@www"});
-//		ab.add(new String[]{"vv", "999", "aaawww"});
-//		ab.add(new String[]{"bbb", "991", "aaa@www"});
-//		ab.add(new String[]{"aab", "996", "rrr@www"});
-//		ab.add(new String[]{"aac", "991", "aaa@www"});
-//		ab.add(new String[]{"cccc", "991", "aaa@www"});
-//		ab.delete("zac");
-//		ab.add(new String[]{"ddd", "991", "aaa@www"});
-//		ab.add(new String[]{"eee", "991", "aaa@www"});
-//		System.out.println(ab.search("aaa"));
-		
-//		ab.printBook();
+				} catch (IOException e) {
+					System.out.println("Failed to delete contact.");
+				}
+
+				System.out.println("Contact " + name + " not found.");
+				continue;
+			}
+
+			if (s.equals("get")) {
+				ab.printBook();
+				continue;
+			}
+
+			if (s.startsWith("search")) {
+
+				if (s.length() > 6 && s.charAt(6) != ' ') {
+					System.out.println("Unknown command.");
+					printHelp();
+					continue;
+				}
+
+				String name = s.substring(6).trim();
+
+				if (name.equals("")) {
+					System.out.println("Command format: search <name>.");
+					continue;
+				}
+
+				Contact c = ab.search(name);
+
+				if (c == null) {
+					System.out.println("Contact " + name + " not found.");
+					continue;
+				}
+				System.out.println(c.toString());
+				continue;
+			}
+
+			if (s.equals("help")) {
+				printHelp();
+				continue;
+			}
+
+			else {
+				System.out.println("Unknown command.");
+				printHelp();
+			}
+
+		}
 
 	}
 
-	private static String getKbdString()
-	{
+	private static void printHelp() {
+		System.out
+				.println("Available commands (enter parameters without brackets):");
+		System.out.println("add - creates new contact.");
+		System.out
+				.println("delete <name> - deletes specified contact from addressbook.");
+		System.out.println("search <name> - searches for specified contact.");
+		System.out.println("get - shows list of all contacts.");
+		System.out.println("exit - exits program.");
+	}
+
+	private static String getKbdString() {
 
 		byte bKbd[] = new byte[256];
 		int count = 0;
-	
-		try
-		{
-		  count = System.in.read(bKbd);
+
+		try {
+			count = System.in.read(bKbd);
+		} catch (Exception ex) {
+			System.out.println(ex.toString());
 		}
-		catch(Exception ex)
-		{
-		  System.out.println(ex.toString()); 
-		}
-	
+
 		return new String(bKbd, 0, count).trim();
+//		StringBuilder sb = new StringBuilder();
+//		
+//		try {
+//			while(System.in.available() > 0) {
+//				System.out.println("available " + System.in.available());
+//				sb.append(System.in.read());
+//				
+//			}
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		return sb.toString();
 	}
-	
+
 }
